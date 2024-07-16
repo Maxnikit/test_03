cbce350cd6405a09c82eab812d7d65cd3c96f35d
Maxnikit <max.nikit.03@gmail.com>
2024-07-16 21:57:16 +0300

    Add Tanstack Query

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "@mantine/core/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { MantineProvider } from "@mantine/core";
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MantineProvider>
-      <App />
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider>
+        <App />
+      </MantineProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
