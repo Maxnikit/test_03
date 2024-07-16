3a813b85b135859b1e12df679a233356268a3765
Maxnikit <max.nikit.03@gmail.com>
2024-07-16 22:15:49 +0300

    add an api call

---
+++ src/api/users.ts
@@ -0,0 +1,11 @@
+import { User } from "../types";
+
+// Gets data about first 9 users
+export async function fetchUsers(): Promise<User[]> {
+  const response = await fetch("https://fakestoreapi.com/users?limit=9");
+  if (!response.ok) {
+    throw new Error("Problem fetching users");
+  }
+  const users: User[] = await response.json();
+  return users;
+}
