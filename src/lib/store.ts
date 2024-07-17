4697c5378a181fca54ba8e1e8c2364c6ad63cd1f
Maxnikit <max.nikit.03@gmail.com>
2024-07-17 04:39:44 +0300

    Add Zustand store for storing userData

---
+++ src/lib/store.ts
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+import { User } from "../types";
+
+interface UserStore {
+  users: User[];
+
+  updateUsers: (newUsers: User[]) => void;
+}
+export const useStore = create<UserStore>((set) => ({
+  users: [],
+
+  updateUsers: (newUsers) => set({ users: newUsers }),
+}));
